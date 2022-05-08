fd8e96a00d4a1f1a2859caea29b4973194e7d3c5
matildamort <matilda.jane.morton@gmail.com>
2022-05-09 07:17:47 +1000

    create unique code for rooms

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import string
+import random
+
+#creates a unique code, with the length of 6. If the code is not unique, it will continue to iterate through until a unique code is created.  
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercaser, k=length))
+        if Room.objects.filter(code=code).count() == 0 :
+            break
+        
+
 
 # Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=10, default ="" , unique = True)
+    #Creates host and limits each room to only have one host per room.
+    host = models.CharField(max_length=50, unique = True)
+    guest_can_pause = models.BooleanField(null=False, default = False)
+    votes_to_skip = models.IntegerField(null=False, default =1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
